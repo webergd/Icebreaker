6609fe03d82e15e6dd6445aedd8809f07edf720a
Mahmudul Hasan <mhasan341@gmail.com>
2022-09-20 23:10:21 +0600

    added Combine

--- Tangerine/controller/auth/UserNameVC.swift
+++ Tangerine/controller/auth/UserNameVC.swift
@@ -9,6 +9,7 @@ import UIKit
 import MaterialComponents
 import FirebaseFirestore
 import FirebaseAuth
+import Combine
 
 // this enum will make life easier while showing the
 // availibility of username
@@ -53,6 +54,7 @@ class UserNameVC: UIViewController, UITextFieldDelegate,UITextViewDelegate {
     var isUserAvailable = false
     // the selected username
     var user_name = ""
+    @Published var usernameEntered = ""
     
     // MARK: Actions
 
@@ -313,6 +315,7 @@ class UserNameVC: UIViewController, UITextFieldDelegate,UITextViewDelegate {
         }else{
             showError()
         }
+      self.usernameEntered = textField.text ?? ""
     }
     
     // when tos/pp clicked
@@ -334,6 +337,20 @@ class UserNameVC: UIViewController, UITextFieldDelegate,UITextViewDelegate {
         return true
     }
     // MARK: VC Methods
+
+  /// validates our currency input and updates the UI according to the value
+  var usernameText: AnyPublisher<String, Never> {
+    return $usernameEntered.map { value in
+      guard value.count >= 3 else {
+        return ""
+      }
+
+      return value
+    }
+    .eraseToAnyPublisher()
+  } // end of validateCurrencyAmount
+  // to store our cancellables
+  private var cancellables = Set<AnyCancellable>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -375,6 +392,18 @@ class UserNameVC: UIViewController, UITextFieldDelegate,UITextViewDelegate {
         
         //dismiss keyboard, call the extension
         hideKeyboardOnOutsideTouch()
+
+
+      // Combine
+      usernameText
+        .debounce(for: .milliseconds(600), scheduler: RunLoop.main)
+        .filter {
+          print($0)
+          return $0.count >= 3
+        }
+        .sink { _ in
+          self.checkAvailability()
+        }.store(in: &cancellables)
     }
     
     
