cab3f79ba75339e9b8d0a2d740c129a21793e0e7
Mahmudul Hasan <mhasan341@gmail.com>
2023-06-23 01:33:14 +0600

    Realm migration updated to 4

--- Tangerine/support/AppDelegate.swift
+++ Tangerine/support/AppDelegate.swift
@@ -38,7 +38,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate {
                   // Set the new schema version. This must be greater than the previously used
                   // version (if you've never set a schema version before, the version is 0).
           // changed to 3: adminReviewRequired added
-                  schemaVersion: 3,
+            // changed to 4: usersNotConsumedBy and isSeeder added
+                  schemaVersion: 4,
 
                   // Set the block which will be called automatically when opening a Realm with
                   // a schema version lower than the one set above
